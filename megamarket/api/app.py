ba5266c13d182af13061cd62d1e1785a4e25d9b8
Dest0re <klacik2002@yandex.ru>
2022-06-24 00:07:51 +0500

    Add swagger docs generation

--- megamarket/api/app.py
+++ megamarket/api/app.py
@@ -8,6 +8,7 @@ from configargparse import Namespace
 from aiohttp import PAYLOAD_REGISTRY
 from aiohttp.web import Application
 from aiohttp_apispec import validation_middleware, setup_aiohttp_apispec
+from aiohttp_swagger import setup_swagger
 
 from megamarket.utils.pg import setup_pg
 from megamarket.api.handlers import HANDLERS
@@ -19,6 +20,12 @@ MAX_REQUEST_SIZE = 5 * MEGABYTE
 
 
 log = logging.getLogger(__name__)
+
+
+async def swagger(app):
+    setup_swagger(
+        app=app, swagger_url='/doc', swagger_info=app['swagger_dict']
+    )
 
 
 def create_app(args: Namespace) -> Application:
@@ -33,8 +40,10 @@ def create_app(args: Namespace) -> Application:
         log.debug('Registering handler: %r as %r', handler, handler.URL_PATH)
         app.router.add_route('*', handler.URL_PATH, handler)
 
-    setup_aiohttp_apispec(app=app, title='Routemaster API', swagger_path='/',
+    setup_aiohttp_apispec(app=app, title='Megamarket API',
                           error_callback=handle_validation_error)
+
+    app.on_startup.append(swagger)
 
     PAYLOAD_REGISTRY.register(JsonPayload, (Mapping, MappingProxyType))
     PAYLOAD_REGISTRY.register(AsyncStreamJsonPayload, (AsyncGeneratorType, AsyncIterable))
