ec8d6e345dd975e2bf9e93ea5057c3f88bd5201d
Dest0re <klacik2002@yandex.ru>
2022-06-23 22:33:25 +0500

    Remove echo handler

--- megamarket/api/handlers/__init__.py
+++ megamarket/api/handlers/__init__.py
@@ -1,8 +1,7 @@
-from .echo import EchoView
 from .imports import ImportsView
 from .delete import DeleteView
 from .nodes import NodesView
 from .sales import SalesView
 from .node import NodeView
 
-HANDLERS = (EchoView, ImportsView, DeleteView, NodesView, SalesView, NodeView)
+HANDLERS = (ImportsView, DeleteView, NodesView, SalesView, NodeView)
